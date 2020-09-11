d2f2aeaeb6e3a0052e52a8e059d7b160f948d11e
Jeff Kim <kim2jy@hotmail.com>
2020-09-11 16:08:30 -0700

    Add functionality to automatically set the number of pints remaining on new keg creation

--- src/components/KegControl.js
+++ src/components/KegControl.js
@@ -2,8 +2,6 @@ import React from "react";
 import NewKegForm from "./NewKegForm";
 import KegList from"./KegList";
 import KegDetail from "./KegDetail";
-import EditKegForm from "./EditKegForm";
-import KegDetail from "./KegDetail";
 
 class KegControl extends React.Component {
   
