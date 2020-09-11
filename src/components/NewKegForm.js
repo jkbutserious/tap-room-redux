c2b9b733e8c105d24a514e7a5c32397a578cd990
Jeff Kim <kim2jy@hotmail.com>
2020-09-11 14:06:28 -0700

    Add form to NewKegForm.js

--- src/components/NewKegForm.js
+++ src/components/NewKegForm.js
@@ -1,10 +1,30 @@
 import React from "react";
+import {v4} from "uuid";
 
 function NewKegForm(props){
+
+  function handleNewKegFormSubmission(event){
+    event.preventDefault();
+    console.log(event.target.names.value);
+    console.log(event.target.location.value);
+    console.log(event.target.issue.value);
+  }
   return (
     <React.Fragment>
-      <h3>This is a form.</h3>
-    </React.Fragment>
+    <form onSubmit={handleNewKegFormSubmission}>
+    <input
+      type='text'
+      name='names'
+      placeholder='Pair Names' />
+    <input
+      type='text'
+      name='location'
+      placeholder='Location' />
+    <textarea
+      name='issue'
+      placeholder='Describe your issue.' />
+    <button type='submit'>Help!</button>
+  </form>    </React.Fragment>
   );
 }
 
