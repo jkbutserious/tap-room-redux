57a8f73e00b74217697f6ffa37c97f843283f315
Jeff Kim <kim2jy@hotmail.com>
2020-09-18 16:25:48 -0700

    Fixed import statements

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
-import reducer from '.reducers.keg-list-reducer';
+import { Provider } from 'react-redux';
+import reducer from './reducers/keg-list-reducer';
 import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
