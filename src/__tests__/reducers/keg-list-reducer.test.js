e8e55266eac69d204966f2e4bfc9bc3cce1a3b34
Jeff Kim <kim2jy@hotmail.com>
2020-09-18 08:50:45 -0700

    Failed first test of keg list reducer

--- src/__tests__/reducers/keg-list-reducer.test.js
+++ src/__tests__/reducers/keg-list-reducer.test.js
@@ -1,4 +1,4 @@
-import kegListReducer from '../../reducers/keg-list-reducer';
+import kegListReducer from '../../reducers/keg-list-reducer.js';
 
 describe('kegListReducer', () => {
   test('Should return default state if there is no action type passed into the reducer', () => {
